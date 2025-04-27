cd5ee5577205927524c7d7eaa926b8676b1f8be1
wwwalohacampus <h850415@naver.com>
2025-04-27 23:31:25 +0900

    deploy

---
+++ deploy/install.bat
@@ -0,0 +1,20 @@
+@echo off
+setlocal
+
+echo [INFO] Creating installer for Board...
+
+jpackage ^
+  --type exe ^
+  --input . ^
+  --name AutoCheck ^
+  --main-jar AutoCheck.jar ^
+  --main-class com.aloha.Main ^
+  --java-options "--module-path app/lib/javafx-sdk-23.0.2/lib --add-modules javafx.controls,javafx.fxml -Xmx512m -Djava.awt.headless=false" ^
+  --icon Auto.ico ^
+  --win-dir-chooser ^
+  --win-menu ^
+  --win-shortcut
+
+echo [INFO] Installer build complete.
+
+pause
